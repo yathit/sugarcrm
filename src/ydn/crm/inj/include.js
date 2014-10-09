2c1000204454cf4f401c4ffc7ea569e24da58b8d
Kyaw Tun <kyawtun@yathit.com>
2014-10-09 15:56:06 +0800

    reorg

--- src/ydn/crm/inj/include.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Load require modules for raw mode.
- */
-
-goog.require('ydn.crm.inj.App');
-goog.require('ydn.crm.ui.SidebarPanel');
-
-
-
--- src/ydn/crm/inj/inj.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kyawtun on 21/12/13.
- */
-
-goog.provide('ydn.crm.inj');
-goog.require('ydn.crm.base');
-
-
-
