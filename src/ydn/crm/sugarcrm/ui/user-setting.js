f9fe12dc67daba62b0be2a2b88bdf7b48cdaf6cc
Kyaw Tun <kyawtun@yathit.com>
2014-10-11 23:39:38 +0800

    seperate app.

--- src/ydn/crm/sugarcrm/ui/user-setting.js
+++ src/ydn/crm/sugarcrm/ui/user-setting.js
@@ -537,8 +537,8 @@ ydn.crm.ui.UserSetting.prototype.show = function() {
 ydn.crm.ui.UserSetting.hasFeature = function(feature) {
   var features = [];
   var app_name = ydn.crm.UserSetting.getAppShortName();
-  if (feature == ydn.crm.base.Feature.EMAIL_TRACKING) {
-    return app_name == ydn.crm.base.AppShortName.EMAIL_TRACKER;
+  if (feature == ydn.crm.base.Feature.TRACKING) {
+    return true;
   } else if (feature == ydn.crm.base.Feature.SUGARCRM) {
     return app_name == ydn.crm.base.AppShortName.SUGARCRM;
   } else if (feature == ydn.crm.base.Feature.GDATA_CONTACT) {
